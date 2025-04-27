eea48b205e4fcf2ac23727acd705df046b984b6b
Hasan Fatih Öztürk <137945674+HasanF19@users.noreply.github.com>
2025-04-28 01:18:47 +0300

    Update Simulation_Interface.py
    Little Update

--- QuantumInsight/Simulation_Interface.py
+++ QuantumInsight/Simulation_Interface.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 from iointel import Agent, Workflow
 
 # API anahtarı
-os.environ["IOINTEL_API_KEY"] = "io-v2-eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJvd25lciI6ImU0MTNlYTcwLTc2ZDMtNGViYi05ZjRlLTU0OTJkYzhlZjM5MCIsImV4cCI6NDg5OTI2MjQ5Nn0.ka9Nog3-HHDxowTRRpns3Y3O5y_K4z-GxZXUxlc-qeY4j__ZD8hy0ufDzh4Ykj1BGyqoNZ0Ho0IsPPpxSPGNbg"
+os.environ["IOINTEL_API_KEY"] = "Apı keyinizi buraya yazın"
 # --------------------------------------------------
 # Proje: Nested Quantum Simülasyon + Multi-Agent Analiz+ Kullanıcı dostu bir arayüz
 # Dosya: Simulation_Interface.py
